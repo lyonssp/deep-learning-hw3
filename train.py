0c0d727d023982a69d31abcf8de6b9c8dd76887a
sean <lyonssp777@gmail.com>
2024-10-31 21:19:33 -0500

    update training harness

--- train.py
+++ train.py
@@ -4,4 +4,6 @@ from homework.train_detection import train as train_detector
 if __name__ == '__main__':
     # train_classifier(lr=2e-3)
 
-    train_detector(lr=2e-3, batch_size = 128, num_epoch=1)
+    train_detector(lr=2e-2, batch_size = 128, num_epoch=20)
+    train_detector(lr=1e-2, batch_size = 128, num_epoch=20)
+    train_detector(lr=2e-3, batch_size = 128, num_epoch=20)
